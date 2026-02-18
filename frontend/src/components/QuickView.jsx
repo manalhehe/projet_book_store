73920d3edd46f776ccab533b8804a7d9aa4917c6
hajar <elberguihajar18@gmail.com>
2026-02-18 14:37:23 +0100

    versionfinale

--- frontend/src/components/QuickView.jsx
+++ frontend/src/components/QuickView.jsx
@@ -4,27 +4,31 @@ const QuickView = ({ book, onClose, onAddToCart }) => {
   return (
     <div style={{
       position: 'fixed', top: 0, left: 0, width: '100%', height: '100%',
-      backgroundColor: 'rgba(208, 219, 205, 0.7)', zIndex: 2000,
+      backgroundColor: 'rgba(255, 255, 255, 0.7)', zIndex: 2000,
       display: 'flex', justifyContent: 'center', alignItems: 'center',
-      backdropFilter: 'blur(5px)' 
+      backdropFilter: 'blur(5px)' //flou de 5px de l'arrière plan , contenu derrière
+      //Cliquer sur le fond ferme le modal
     }} onClick={onClose}> 
       
       <div 
       //événement React qui se déclenche quand la souris quitte l’élément.
+      //Sans ça, cliquer n'importe où dans le modal le fermerait immédiatement.
         onClick={(e) => e.stopPropagation()} 
         style={{
           background: '#ffffffff', width: '90%', maxWidth: '800px', borderRadius: '25px',
           display: 'flex', overflow: 'hidden', position: 'relative', 
         }}
       >
+        
 <button onClick={onClose} style={{ position: 'absolute', top: '20px', right: '20px', background: 'none', border: 'none', fontSize: '1.5rem', cursor: 'pointer' }}>✕</button>
         
-        {/* Left Side: Image */}
+        {/* Left Side: Image : Ce côté prend 1 part de l'espace disponible. */}
         <div style={{ flex: 1, background: '#e7e0e0ff', padding: '40px', display: 'flex', alignItems: 'center' }}>
           <img src={book.image} alt={book.title} style={{ width: '100%', borderRadius: '10px', boxShadow: '0 10px 30px rgba(0,0,0,0.1)' }} />
         </div>
 
-        {/* Right Side: Details */}
+        {/* Right Side: Details Ce côté prend 1.2 parts vs 1 part pour l'image */}
+        {/*URL dynamique*/ }
         <div style={{ flex: 1.2, padding: '50px' }}>
           <p style={{ color: '#000000ff', fontWeight: 'bold', textTransform: 'uppercase', fontSize: '0.8rem' }}>{book.genre}</p>
           <h2 style={{ fontSize: '2rem', margin: '10px 0' }}>{book.title}</h2>
